4cacb2e802fad9de56866a730679f10e335e5638
SehajdeepSingh95 <sehajdeepsdsn@gmail.com>
2025-04-18 14:35:29 +0530

    Update Ascending_Descending.py

--- Ascending_Descending.py
+++ Ascending_Descending.py
@@ -1,24 +1,25 @@
 total_numbers=int(input("Please enter how many numbers you wanna sort: "))
 NumberList  = []
 j=0
-order=input("in which order do you wanna sort, Ascending or Descending: ")
-if(order.lower()=="ascending" or order.lower() == "ascendings"):
-    while (len(NumberList) <= total_numbers - 1):
+order=input("in which order do you wanna sort, ascending or descending: ")
+if order.lower()=="ascending" or order.lower() == "ascendings":
+    while len(NumberList) <= total_numbers - 1:
         i = int(input("Please enter the number"))
         NumberList.insert(j, i)
         j = j + 1
 
     NumberList.sort()
     print(NumberList)
-elif(order.lower() == "descending" or order.lower() == "descendings"):
-        while (len(NumberList) <= total_numbers - 1):
+elif order.lower() == "descending" or order.lower() == "descendings":
+        while len(NumberList) <= total_numbers - 1:
             i = int(input("Please enter the number"))
             NumberList.insert(j, i)
             j = j + 1
 
         NumberList.sort()
         NumberList.reverse()
-        print(NumberList)
+        numberlist=str(NumberList)
+        print(numberlist)
 else:
     print("Only inputs ascending and descending are allowed")
 
