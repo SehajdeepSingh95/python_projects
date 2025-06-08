65f2d1cfe788e1b4a0ef51e7a30287ea6f3a607c
SehajdeepSingh95 <sehajdeepsdsn@gmail.com>
2025-06-08 20:35:56 +0530

    Update Ascending_Descending.py

--- Ascending_Descending.py
+++ Ascending_Descending.py
@@ -1,22 +1,21 @@
 total_numbers=int(input("Please enter how many numbers you wanna sort: "))
 NumberList  = []
 j=0
-order=input("in which order do you wanna sort, ascending or descending: ")
-if order.lower()=="ascending" or order.lower() == "ascendings":
+order=input("in which order do you wanna sort, \"a\" for ascending and \"d\" for descending: ")
+if order.lower()=="ascending" or order.lower() == "ascendings" or order.lower()=="a":
     while len(NumberList) <= total_numbers - 1:
-        i = int(input("Please enter the number"))
+        i = int(input("Please enter the number:"))
         NumberList.insert(j, i)
         j = j + 1
 
     NumberList.sort()
     print(NumberList)
-elif order.lower() == "descending" or order.lower() == "descendings":
+elif order.lower() == "descending" or order.lower() == "descendings" or order.lower()=="d":
         while len(NumberList) <= total_numbers - 1:
-            i = int(input("Please enter the number"))
+            i = int(input("Please enter the number:"))
             NumberList.insert(j, i)
             j = j + 1
-
-         NumberList.sort(reverse=True)
+        NumberList.sort(reverse=True)
         #OR
         # NumberList.sort()
         # NumberList.reverse()
@@ -24,4 +23,3 @@ elif order.lower() == "descending" or order.lower() == "descendings":
         print(numberlist)
 else:
     print("Only inputs ascending and descending are allowed")
-
